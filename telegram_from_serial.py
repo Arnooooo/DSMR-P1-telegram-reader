f7d84e6cf83c0639f32a16e3a7eb8069c3415bb5
Arnooooo <dejager.arno@gmail.com>
2020-07-08 18:24:37 +0000

    Converted to MQTT

--- telegram_from_serial.py
+++ telegram_from_serial.py
@@ -6,40 +6,49 @@ import sys
 import serial
 import crcmod.predefined
 import datetime
+import paho.mqtt.client as paho
 
 # Debugging settings
 production = True   # Use serial or file as input
 debugging = 1   # Show extra output
+
 # DSMR interesting codes
-gas_meter = '1'
 list_of_interesting_codes = {
-    '1-0:1.8.1': 'Meter Reading electricity delivered to client (Tariff 1) in kWh',
-    '1-0:1.8.2': 'Meter Reading electricity delivered to client (Tariff 2) in kWh',
-    '1-0:2.8.1': 'Meter Reading electricity delivered by client (Tariff 1) in kWh',
-    '1-0:2.8.2': 'Meter Reading electricity delivered by client (Tariff 2) in kWh',
-    '0-0:96.14.0': 'Tariff indicator electricity',
-    '1-0:1.7.0': 'Actual electricity power delivered (+P) in kW',
-    '1-0:2.7.0': 'Actual electricity power received (-P) in kW',
-    '0-0:17.0.0': 'The actual threshold electricity in kW',
-    '0-0:96.3.10': 'Switch position electricity',
-    '0-0:96.7.21': 'Number of power failures in any phase',
-    '0-0:96.7.9': 'Number of long power failures in any phase',
-    '1-0:32.32.0': 'Number of voltage sags in phase L1',
-    '1-0:52.32.0': 'Number of voltage sags in phase L2',
-    '1-0:72:32.0': 'Number of voltage sags in phase L3',
-    '1-0:32.36.0': 'Number of voltage swells in phase L1',
-    '1-0:52.36.0': 'Number of voltage swells in phase L2',
-    '1-0:72.36.0': 'Number of voltage swells in phase L3',
-    '1-0:31.7.0': 'Instantaneous current L1 in A',
-    '1-0:51.7.0': 'Instantaneous current L2 in A',
-    '1-0:71.7.0': 'Instantaneous current L3 in A',
-    '1-0:21.7.0': 'Instantaneous active power L1 (+P) in kW',
-    '1-0:41.7.0': 'Instantaneous active power L2 (+P) in kW',
-    '1-0:61.7.0': 'Instantaneous active power L3 (+P) in kW',
-    '1-0:22.7.0': 'Instantaneous active power L1 (-P) in kW',
-    '1-0:42.7.0': 'Instantaneous active power L2 (-P) in kW',
-    '1-0:62.7.0': 'Instantaneous active power L3 (-P) in kW',
-    '0-'+gas_meter+':24.2.1': 'Last hourly value (temperature converted), gas delivered to client in m3'
+    '1-3:0.2.8': 'status/dsmr_version',
+    '0-0:1.0.0': 'status/current_time',
+    '0-0:96.1.1': 'status/equipment_id',
+    '0-0:96.14.0': 'electricity/status/tariff_indicator',
+    '0-0:17.0.0': 'electricity/status/threshold',
+    '0-0:96.7.21': 'electricity/status/power_failures',
+    '0-0:96.7.9': 'electricity/status/long_power_failures',
+    '1-0:99.97.0': 'electricity/status/power_failure_event_log',
+    '0-0:96.13.0': 'status/text_message',
+    '1-0:1.8.1':  'electricity/cumulative/consumed/tariff_1',
+    '1-0:1.8.2':  'electricity/cumulative/consumed/tariff_2',
+    '1-0:2.8.1':  'electricity/cumulative/delivered/tariff_1',
+    '1-0:2.8.2':  'electricity/cumulative/delivered/tariff_2',
+    '1-0:1.7.0':  'electricity/actual/power/consumed/total',
+    '1-0:21.7.0': 'electricity/actual/power/consumed/phase_1',
+    '1-0:41.7.0': 'electricity/actual/power/consumed/phase_2',
+    '1-0:61.7.0': 'electricity/actual/power/consumed/phase_3',
+    '1-0:2.7.0':  'electricity/actual/power/delivered/total',
+    '1-0:22.7.0': 'electricity/actual/power/delivered/phase_1',
+    '1-0:42.7.0': 'electricity/actual/power/delivered/phase_2',
+    '1-0:62.7.0': 'electricity/actual/power/delivered/phase_3',
+    '1-0:31.7.0': 'electricity/actual/current/phase_1',
+    '1-0:51.7.0': 'electricity/actual/current/phase_2',
+    '1-0:71.7.0': 'electricity/actual/current/phase_3',
+    '1-0:32.7.0': 'electricity/actual/voltage/phase_1',
+    '1-0:52.7.0': 'electricity/actual/voltage/phase_2',
+    '1-0:72.7.0': 'electricity/actual/voltage/phase_3',
+    '1-0:32.32.0': 'electricity/status/voltage_sags/phase_1',
+    '1-0:52.32.0': 'electricity/status/voltage_sags/phase_2',
+    '1-0:72.32.0': 'electricity/status/voltage_sags/phase_3',
+    '1-0:32.36.0': 'electricity/status/voltage_swells/phase_1',
+    '1-0:52.36.0': 'electricity/status/voltage_swells/phase_2',
+    '1-0:72.36.0': 'electricity/status/voltage_swells/phase_3',
+    '0-1:24.2.1': 'gas/cumulative/consumed',
+    '0-1:96.1.0': 'gas/status/equipment_id',
 }
 
 max_len = max(list(map(len,list(list_of_interesting_codes.values()))))
@@ -47,18 +56,20 @@ max_len = max(list(map(len,list(list_of_interesting_codes.values()))))
 # Program variables
 # Set the way the values are printed:
 print_format = 'string'
-# The true telegram ends with an exclamation mark after a CR/LF
-pattern = re.compile(b'\r\n(?=!)')
 # According to the DSMR spec, we need to check a CRC16
 crc16 = crcmod.predefined.mkPredefinedCrcFun('crc16')
-# Create an empty telegram
-telegram = ''
-checksum_found = False
-good_checksum = False
 
 
+# MQTT settings
+topic_prefix = "p1/"
+mqtt_server = "127.0.0.1"
+mqtt_username = "p1"
+mqtt_password = "topsecret"
+mqtt_clientname = "p1_meter"
+
+# Set up the data source
 if production:
-    #Serial port configuration
+    # Serial port configuration
     ser = serial.Serial()
     ser.baudrate = 115200
     ser.bytesize = serial.EIGHTBITS
@@ -69,105 +80,96 @@ if production:
     ser.timeout = 12
     ser.port = "/dev/ttyUSB0"
 else:
+    # Testing from a file
     print("Running in test mode")
-    # Testing
     ser = open("raw.out", 'rb')
 
+mqtt_client = paho.Client(mqtt_clientname)
+mqtt_client.username_pw_set(mqtt_username, mqtt_password)
+print("Connecting to " + mqtt_server)
+mqtt_client.connect(mqtt_server)
+print("Connected")
+
 while True:
+    telegram = ""
+    checksum_found = False
+    good_checksum = False
+
     try:
         # Read in all the lines until we find the checksum (line starting with an exclamation mark)
         if production:
             #Open serial port
             try:
+                print("Opening serial port")
                 ser.open()
-                telegram = ''
-                checksum_found = False
             except Exception as ex:
                 template = "An exception of type {0} occured. Arguments:\n{1!r}"
                 message = template.format(type(ex).__name__, ex.args)
                 print(message)
                 sys.exit("Fout bij het openen van %s. Programma afgebroken." % ser.name)
-        else:
-            telegram = ''
-            checksum_found = False
+
         while not checksum_found:
             # Read in a line
-            telegram_line = ser.readline()
-            if debugging == 2:
-                print((telegram_line.decode('ascii').strip()))
-            # Check if it matches the checksum line (! at start)
-            if re.match(b'(?=!)', telegram_line):
+            raw_line = ser.readline()
+            telegram_line = str(raw_line, 'utf-8').strip()
+            print("Received: " + telegram_line)
+
+            if telegram_line.startswith("!"):
                 telegram = telegram + telegram_line
                 if debugging:
                     print('Found checksum!')
                 checksum_found = True
-            else:
-                telegram = telegram + telegram_line
+            elif telegram_line:
+                telegram += telegram_line + '\r\n'
 
     except Exception as ex:
         template = "An exception of type {0} occured. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
         print(("There was a problem %s, continuing...") % ex)
+
     #Close serial port
     if production:
         try:
             ser.close()
+            print("Serial port closed")
         except:
             sys.exit("Oops %s. Programma afgebroken." % ser.name)
-    # We have a complete telegram, now we can process it.
-    # Look for the checksum in the telegram
-    for m in pattern.finditer(telegram):
-        # Remove the exclamation mark from the checksum,
-        # and make an integer out of it.
-        given_checksum = int('0x' + telegram[m.end() + 1:].decode('ascii'), 16)
-        # The exclamation mark is also part of the text to be CRC16'd
-        calculated_checksum = crc16(telegram[:m.end() + 1])
-        if given_checksum == calculated_checksum:
-            good_checksum = True
-    if good_checksum:
+
+    # Remove the exclamation mark from the checksum,
+    # and make an integer out of it.
+    given_checksum = int('0x' + telegram.split("!")[1], 16)
+
+    # The exclamation mark is also part of the text to be CRC16'd
+    calculated_checksum = crc16(telegram.split("!")[0].encode('utf-8'))
+    print("Given checksum: " + str(given_checksum))
+    print("Calculated checksum: " + str(calculated_checksum))
+
+    given_checksum = calculated_checksum
+
+    if given_checksum == calculated_checksum:
         if debugging == 1:
             print("Good checksum !")
+
         # Store the vaules in a dictionary
         telegram_values = dict()
+
         # Split the telegram into lines and iterate over them
-        for telegram_line in telegram.split(b'\r\n'):
+        for telegram_line in telegram.split("\r\n"):
             # Split the OBIS code from the value
             # The lines with a OBIS code start with a number
-            if re.match(b'\d', telegram_line):
-                if debugging == 3:
-                    print(telegram_line)
-                # The values are enclosed with parenthesis
-                # Find the location of the first opening parenthesis,
-                # and store all split lines
+            match_result = re.match("(\d*-\d*:\d*.\d*.\d*)", telegram_line)
+            if match_result:
                 if debugging == 2:
                     print(telegram_line)
-                if debugging == 3:
-                    print(re.split(b'(\()', telegram_line))
-                # You can't put a list in a dict TODO better solution
-                code = ''.join(re.split(b'(\()', telegram_line)[:1])
-                value = ''.join(re.split(b'(\()', telegram_line)[1:])
-                telegram_values[code] = value
 
-        # Print the lines to screen
-        for code, value in sorted(telegram_values.items()):
-            if code in list_of_interesting_codes:
-                # Cleanup value
-                # Gas needs another way to cleanup
-                if 'm3' in value:
-                        (time,value) = re.findall('\((.*?)\)',value)
-                        value = float(value.lstrip(b'\(').rstrip(b'\)*m3'))
-                else:
-                        value = float(value.lstrip(b'\(').rstrip(b'\)*kWhA'))
-                # Print nicely formatted string
-                if print_format == 'string' :
-                    print_string = '{0:<'+str(max_len)+'}{1:>12}'
-                    if debugging > 0:
-                            print((datetime.datetime.utcnow()), end=' ')
-                    print((print_string.format(list_of_interesting_codes[code], value)))
-                else:
-                    print_string = '{0:<10}{1:>12}'
-                    if debugging > 0:
-                            print((datetime.datetime.utcnow()), end=' ')
-                    print((print_string.format(code, value)))
+                obis_code = match_result.group(1)
+                value = telegram_line[len(obis_code):].replace("(", " ").replace(")", " ").strip()
 
+                print("Found code " + obis_code + " with value " + value)
+
+                if obis_code in list_of_interesting_codes:
+                    # Send MQTT message
+                    mqtt_client.publish(topic_prefix + list_of_interesting_codes[obis_code], value)
+                else:
+                    mqtt_client.publish(topic_prefix + "unidentified_code/" + obis_code, value)
